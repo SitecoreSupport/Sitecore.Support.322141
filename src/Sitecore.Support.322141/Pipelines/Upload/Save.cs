c49d879db0f1c2dd9c15bbdaf9a40e1177ff4280
Daniel Sim Yeat Han <dsd@sitecore.net>
2019-04-08 14:29:14 +0800

    First commit for support patch - Sitecore.Support.322141

---
+++ src/Sitecore.Support.322141/Pipelines/Upload/Save.cs
@@ -0,0 +1,161 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: Sitecore.Pipelines.Upload.Save
+// Assembly: Sitecore.Kernel, Version=11.1.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: F8DE3552-BE39-41F4-8D7E-04A0C08DC796
+// Assembly location: C:\inetpub\wwwroot\sc902.local\bin\Sitecore.Kernel.dll
+
+using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using Sitecore.IO;
+using Sitecore.SecurityModel;
+using Sitecore.Web;
+using Sitecore.Zip;
+using Sitecore.Pipelines.Upload;
+using Sitecore.Support.Resources.Media;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Support.Pipelines.Upload
+{
+    /// <summary>Saves the uploaded files.</summary>
+    public class Save : UploadProcessor
+    {
+        /// <summary>Runs the processor.</summary>
+        /// <param name="args">The arguments.</param>
+        /// <exception cref="T:System.Exception"><c>Exception</c>.</exception>
+        public void Process(UploadArgs args)
+        {
+            Assert.ArgumentNotNull((object)args, nameof(args));
+            for (int index = 0; index < args.Files.Count; ++index)
+            {
+                HttpPostedFile file1 = args.Files[index];
+                if (!string.IsNullOrEmpty(file1.FileName))
+                {
+                    try
+                    {
+                        bool flag = UploadProcessor.IsUnpack(args, file1);
+                        if (args.FileOnly)
+                        {
+                            if (flag)
+                            {
+                                Save.UnpackToFile(args, file1);
+                            }
+                            else
+                            {
+                                string file2 = this.UploadToFile(args, file1);
+                                if (index == 0)
+                                    args.Properties["filename"] = (object)FileHandle.GetFileHandle(file2);
+                            }
+                        }
+                        else
+                        {
+                            MediaUploader mediaUploader = new MediaUploader()
+                            {
+                                File = file1,
+                                Unpack = flag,
+                                Folder = args.Folder,
+                                Versioned = args.Versioned,
+                                Language = args.Language,
+                                AlternateText = args.GetFileParameter(file1.FileName, "alt"),
+                                Overwrite = args.Overwrite,
+                                FileBased = args.Destination == UploadDestination.File
+                            };
+                            List<MediaUploadResult> mediaUploadResultList;
+                            using (new SecurityDisabler())
+                                mediaUploadResultList = mediaUploader.Upload();
+                            Log.Audit((object)this, "Upload: {0}", new string[1]
+                            {
+                file1.FileName
+                            });
+                            foreach (MediaUploadResult mediaUploadResult in mediaUploadResultList)
+                                this.ProcessItem(args, (MediaItem)mediaUploadResult.Item, mediaUploadResult.Path);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Log.Error("Could not save posted file: " + file1.FileName, ex, (object)this);
+                        throw;
+                    }
+                }
+            }
+        }
+
+        /// <summary>Processes the item.</summary>
+        /// <param name="args">The arguments.</param>
+        /// <param name="mediaItem">The media item.</param>
+        /// <param name="path">The path.</param>
+        private void ProcessItem(UploadArgs args, MediaItem mediaItem, string path)
+        {
+            Assert.ArgumentNotNull((object)args, nameof(args));
+            Assert.ArgumentNotNull((object)mediaItem, nameof(mediaItem));
+            Assert.ArgumentNotNull((object)path, nameof(path));
+            if (args.Destination == UploadDestination.Database)
+                Log.Info("Media Item has been uploaded to database: " + path, (object)this);
+            else
+                Log.Info("Media Item has been uploaded to file system: " + path, (object)this);
+            args.UploadedItems.Add(mediaItem.InnerItem);
+        }
+
+        /// <summary>Unpacks to file.</summary>
+        /// <param name="args">The arguments.</param>
+        /// <param name="file">The file.</param>
+        private static void UnpackToFile(UploadArgs args, HttpPostedFile file)
+        {
+            Assert.ArgumentNotNull((object)args, nameof(args));
+            Assert.ArgumentNotNull((object)file, nameof(file));
+            string filename = FileUtil.MapPath(TempFolder.GetFilename("temp.zip"));
+            file.SaveAs(filename);
+            using (ZipReader zipReader = new ZipReader(filename))
+            {
+                foreach (ZipEntry entry1 in zipReader.Entries)
+                {
+                    ZipEntry entry = entry1;
+                    if (((IEnumerable<char>)Path.GetInvalidFileNameChars()).Any<char>((Func<char, bool>)(ch => entry.Name.Contains<char>(ch))))
+                    {
+                        string message = string.Format("The \"{0}\" file was not uploaded because it contains malicious file: \"{1}\"", (object)file.FileName, (object)entry.Name);
+                        Log.Warn(message, (object)typeof(Save));
+                        args.UiResponseHandlerEx.MaliciousFile(StringUtil.EscapeJavascriptString(file.FileName));
+                        args.ErrorText = message;
+                        args.AbortPipeline();
+                        return;
+                    }
+                }
+                foreach (ZipEntry entry in zipReader.Entries)
+                {
+                    string str = FileUtil.MakePath(args.Folder, entry.Name, '\\');
+                    if (entry.IsDirectory)
+                    {
+                        Directory.CreateDirectory(str);
+                    }
+                    else
+                    {
+                        if (!args.Overwrite)
+                            str = FileUtil.GetUniqueFilename(str);
+                        Directory.CreateDirectory(Path.GetDirectoryName(str));
+                        lock (FileUtil.GetFileLock(str))
+                            FileUtil.CreateFile(str, entry.GetStream(), true);
+                    }
+                }
+            }
+        }
+
+        /// <summary>Uploads to file.</summary>
+        /// <param name="args">The arguments.</param>
+        /// <param name="file">The file.</param>
+        /// <returns>The name of the uploaded file</returns>
+        private string UploadToFile(UploadArgs args, HttpPostedFile file)
+        {
+            Assert.ArgumentNotNull((object)args, nameof(args));
+            Assert.ArgumentNotNull((object)file, nameof(file));
+            string str = FileUtil.MakePath(args.Folder, Path.GetFileName(file.FileName), '\\');
+            if (!args.Overwrite)
+                str = FileUtil.GetUniqueFilename(str);
+            file.SaveAs(str);
+            Log.Info("File has been uploaded: " + str, (object)this);
+            return Assert.ResultNotNull<string>(str);
+        }
+    }
+}
---
+++ src/Sitecore.Support.322141/Resources/Media/MediaUploader.cs
@@ -0,0 +1,238 @@
+﻿// Sitecore.Resources.Media.MediaUploader
+using Sitecore.Data;
+using Sitecore.Diagnostics;
+using Sitecore.Globalization;
+using Sitecore.IO;
+using Sitecore.Pipelines.GetMediaCreatorOptions;
+using Sitecore.Resources.Media;
+using Sitecore.Zip;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Web;
+
+namespace Sitecore.Support.Resources.Media
+{
+    /// <summary>
+    /// Represents a MediaUpload.
+    /// </summary>
+    public class MediaUploader
+    {
+        private MediaCreator mediaCreator = null;
+
+        private string _alternateText;
+
+        private HttpPostedFile _file;
+
+        private Language _language;
+
+        private string _folder = string.Empty;
+
+        public MediaUploader()
+        {
+            this.mediaCreator = new MediaCreator();
+        }
+
+        /// <summary>
+        /// Gets or sets the alternate text.
+        /// </summary>
+        /// <value>The alternate text.</value>
+        public string AlternateText
+        {
+            get
+            {
+                return this._alternateText;
+            }
+            set
+            {
+                this._alternateText = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the file.
+        /// </summary>
+        /// <value>The file.</value>
+        public HttpPostedFile File
+        {
+            get
+            {
+                return this._file;
+            }
+            set
+            {
+                Assert.ArgumentNotNull(value, "value");
+                this._file = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the folder.
+        /// </summary>
+        /// <value>The folder.</value>
+        public string Folder
+        {
+            get
+            {
+                return this._folder;
+            }
+            set
+            {
+                Assert.ArgumentNotNull(value, "value");
+                this._folder = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this <see cref="T:Sitecore.Resources.Media.MediaUploader" /> is unpack.
+        /// </summary>
+        /// <value><c>true</c> if unpack; otherwise, <c>false</c>.</value>
+        public bool Unpack
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this <see cref="T:Sitecore.Resources.Media.MediaUploader" /> is versioned.
+        /// </summary>
+        /// <value><c>true</c> if versioned; otherwise, <c>false</c>.</value>
+        public bool Versioned
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the language.
+        /// </summary>
+        /// <value>The language.</value>
+        public Language Language
+        {
+            get
+            {
+                return this._language;
+            }
+            set
+            {
+                Assert.ArgumentNotNull(value, "value");
+                this._language = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this <see cref="T:Sitecore.Resources.Media.MediaUploader" /> is overwrite.
+        /// </summary>
+        /// <value><c>true</c> if overwrite; otherwise, <c>false</c>.</value>
+        public bool Overwrite
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the <see cref="T:Sitecore.Resources.Media.MediaUploader" /> files the based.
+        /// </summary>
+        /// <value>
+        /// 	<c>true</c> if the <see cref="T:Sitecore.Resources.Media.MediaUploader" /> files the based; otherwise, <c>false</c>.
+        /// </value>
+        public bool FileBased
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the database to create media item in.
+        /// </summary>
+        /// <value>The database.</value>
+        public Database Database
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Uploads this instance.
+        /// </summary>
+        public List<MediaUploadResult> Upload()
+        {
+            List<MediaUploadResult> list = new List<MediaUploadResult>();
+            if (string.Compare(Path.GetExtension(this.File.FileName), ".zip", StringComparison.InvariantCultureIgnoreCase) == 0 && this.Unpack)
+            {
+                this.UnpackToDatabase(list);
+            }
+            else
+            {
+                this.UploadToDatabase(list);
+            }
+            return list;
+        }
+
+        /// <summary>
+        /// Uploads to database.
+        /// </summary>
+        private void UploadToDatabase(List<MediaUploadResult> list)
+        {
+            Assert.ArgumentNotNull(list, "list");
+            MediaUploadResult mediaUploadResult = new MediaUploadResult();
+            list.Add(mediaUploadResult);
+            mediaUploadResult.Path = FileUtil.MakePath(this.Folder, Path.GetFileName(this.File.FileName), '/');
+            mediaUploadResult.ValidMediaPath = MediaPathManager.ProposeValidMediaPath(mediaUploadResult.Path);
+            MediaCreatorOptions options = new MediaCreatorOptions
+            {
+                Versioned = this.Versioned,
+                Language = this.Language,
+                OverwriteExisting = this.Overwrite,
+                Destination = mediaUploadResult.ValidMediaPath,
+                FileBased = this.FileBased,
+                AlternateText = this.AlternateText,
+                Database = this.Database
+            };
+            options.Build(GetMediaCreatorOptionsArgs.UploadContext);
+            mediaUploadResult.Item = MediaManager.Creator.CreateFromStream(this.File.InputStream, mediaUploadResult.Path, options);
+        }
+
+        /// <summary>
+        /// Unpacks to database.
+        /// </summary>
+        private void UnpackToDatabase(List<MediaUploadResult> list)
+        {
+            Assert.ArgumentNotNull(list, "list");
+            string str = FileUtil.MapPath(TempFolder.GetFilename("temp.zip"));
+            this.File.SaveAs(str);
+            try
+            {
+                using (ZipReader zipReader = new ZipReader(str))
+                {
+                    foreach (ZipEntry entry in zipReader.Entries)
+                    {
+                        if (!entry.IsDirectory)
+                        {
+                            MediaUploadResult mediaUploadResult = new MediaUploadResult();
+                            list.Add(mediaUploadResult);
+                            mediaUploadResult.Path = FileUtil.MakePath(this.Folder, entry.Name, '/');
+                            mediaUploadResult.ValidMediaPath = MediaPathManager.ProposeValidMediaPath(mediaUploadResult.Path);
+                            MediaCreatorOptions options = new MediaCreatorOptions
+                            {
+                                Language = this.Language,
+                                Versioned = this.Versioned,
+                                OverwriteExisting = this.Overwrite,
+                                Destination = mediaUploadResult.ValidMediaPath,
+                                FileBased = this.FileBased,
+                                Database = this.Database
+                            };
+                            options.Build(GetMediaCreatorOptionsArgs.UploadContext);
+                            Stream stream = entry.GetStream();
+                            mediaUploadResult.Item = this.mediaCreator.CreateFromStream(stream, mediaUploadResult.Path, options);
+                        }
+                    }
+                }
+            }
+            finally
+            {
+                FileUtil.Delete(str);
+            }
+        }
+    }
+}
